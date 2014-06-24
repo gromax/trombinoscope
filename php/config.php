073afcb798ee04b2a4778b7e4fe967662127d8ad
gromax <gromaxx@gmail.com>
2014-06-24 22:41:35 +0200

    Ajout des scripts de configuration et une gestion des authorisations en attente pour de nouveaux niveaux d'utilisation

---
+++ php/config.php
@@ -0,0 +1,10 @@
+﻿<?php
+	define("TIME_OUT",5000);
+	define("RANG_SUPER_ADMIN", 10);
+	define("RANG_ADMIN", 7);
+	define("RANG_USER", 6);
+	define("RANG_VISITOR", 5);
+	define("RANG_ANONYME_CONTRIBUTOR", 2);
+	define("PWD_SEED", "@dFf*12");
+
+?>
